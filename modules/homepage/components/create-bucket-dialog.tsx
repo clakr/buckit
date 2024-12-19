46685b1e1f044444f7d4188536a00b1ce3c08a0b
clakr <clarktolosa@gmail.com>
2024-12-20 06:56:19 +0800

    feat(ui): enhance CreateBucketDialog with form fields for name, total amount, and description

--- modules/homepage/components/create-bucket-dialog.tsx
+++ modules/homepage/components/create-bucket-dialog.tsx
@@ -1,8 +1,13 @@
+import { Button } from "@/components/ui/button";
 import {
   DialogHeader,
   DialogTitle,
   DialogDescription,
+  DialogFooter,
 } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function CreateBucketDialog() {
   return (
@@ -15,6 +20,23 @@ export default function CreateBucketDialog() {
           </DialogDescription>
         </DialogHeader>
       </DialogHeader>
+      <form className="grid gap-y-4">
+        <div className="grid gap-y-1.5">
+          <Label htmlFor="name">Name</Label>
+          <Input type="text" name="name" id="name" />
+        </div>
+        <div className="grid gap-y-1.5">
+          <Label htmlFor="totalAmount">Total Amount</Label>
+          <Input type="number" name="totalAmount" id="totalAmount" />
+        </div>
+        <div className="grid gap-y-1.5">
+          <Label htmlFor="description">Description (optional)</Label>
+          <Textarea name="description" id="description" rows={5}></Textarea>
+        </div>
+      </form>
+      <DialogFooter>
+        <Button>Create</Button>
+      </DialogFooter>
     </>
   );
 }
