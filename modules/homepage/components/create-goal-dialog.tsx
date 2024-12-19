d13222a6e579e815b3806068ade5b7284fe8afb4
clakr <clarktolosa@gmail.com>
2024-12-20 07:05:54 +0800

    feat(ui): add form fields for name, total amount, target amount, and description in CreateGoalDialog

--- modules/homepage/components/create-goal-dialog.tsx
+++ modules/homepage/components/create-goal-dialog.tsx
@@ -1,8 +1,13 @@
+import { Button } from "@/components/ui/button";
 import {
   DialogHeader,
   DialogTitle,
   DialogDescription,
+  DialogFooter,
 } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function CreateGoalDialog() {
   return (
@@ -15,6 +20,27 @@ export default function CreateGoalDialog() {
           </DialogDescription>
         </DialogHeader>
       </DialogHeader>
+      <form className="grid gap-y-4">
+        <div className="grid gap-y-1.5">
+          <Label htmlFor="name">Name</Label>
+          <Input type="text" name="name" id="name" />
+        </div>
+        <div className="grid gap-y-1.5">
+          <Label htmlFor="totalAmount">Total Amount</Label>
+          <Input type="number" name="totalAmount" id="totalAmount" />
+        </div>
+        <div className="grid gap-y-1.5">
+          <Label htmlFor="targetAmount">Target Amount</Label>
+          <Input type="number" name="targetAmount" id="targetAmount" />
+        </div>
+        <div className="grid gap-y-1.5">
+          <Label htmlFor="description">Description (optional)</Label>
+          <Textarea name="description" id="description" rows={5}></Textarea>
+        </div>
+      </form>
+      <DialogFooter>
+        <Button>Create</Button>
+      </DialogFooter>
     </>
   );
 }
